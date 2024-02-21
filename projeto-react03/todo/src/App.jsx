4bd6a4612ab911fda1b593573118e3509a217bd3
Walace Rodrigues <walaceclash10@gmail.com>
2024-02-21 16:36:03 -0300

    finish

--- projeto-react03/todo/src/App.jsx
+++ projeto-react03/todo/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Todo from './components/Todo';
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
 import './App.css';
 
 function App() {
@@ -25,15 +26,43 @@ function App() {
     },
   ])
 
+  const [search, setSearch] = useState("")
+
+  const addTodo = (text, category) => {
+    const newTodos = [...todos, {
+      id: Math.floor(Math.random() * 10000),
+      text,
+      category,
+      isCompleted: false,
+    }];
+    setTodos(newTodos);
+  }
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null)
+    setTodos(filteredTodos)
+  };
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
+    setTodos(newTodos)
+  }
+
   return <div className="app">
     <h1>Lista de Tarefas</h1>
+    <Search search={search} setSearch={setSearch} />
     <div className="todo-list">
-      {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} />
+      {todos.filter((todo) => 
+      todo.text.toLowerCase().includes(search.toLowerCase())
+      )
+      .map((todo) => (
+        <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
       ))
       }
     </div>
-    <TodoForm />
+    <TodoForm addTodo={addTodo} />
   </div>
 }
 
--- projeto-react03/todo/src/components/TodoForm.jsx
+++ projeto-react03/todo/src/components/TodoForm.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 
-const TodoForm = () => {
+const TodoForm = ({addTodo}) => {
 
    const [value,setValue] = useState("");
    const [category, setCategory] = useState("");
@@ -8,7 +8,7 @@ const TodoForm = () => {
    const handleSubmit = (e) => {
      e.preventDefault()
      if(!value || !category) return;
-     // add todo
+     addTodo(value, category)
      setValue("");
      setCategory("");
 
